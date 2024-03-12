3f97ba6a48f0fc72155ab0fcccd076e19ee6a3a3
moshuss <113644739+moshuss@users.noreply.github.com>
2024-03-11 21:46:18 -0700

    add Project class and updated projectPage and create saveEdits file

---
+++ saveEdits.php
@@ -0,0 +1,14 @@
+<?php
+include 'Project.php';
+$pCode = filter_input(INPUT_GET, 'project_code');
+$project = new Project($pCode);
+
+$project->title = filter_input(INPUT_GET, 'title');
+$project->description = filter_input(INPUT_GET, 'description');
+$project->stage = filter_input(INPUT_GET, 'stage');
+$project->type = filter_input(INPUT_GET, 'type');
+$project->manager = filter_input(INPUT_GET, 'project_manager');
+$project->startDate= filter_input(INPUT_GET, 'start_date');
+$project->endDate = filter_input(INPUT_GET, 'end_date');
+$project->updateProjectData($pCode);
+?>
