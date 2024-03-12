5b81a1104d7412131d19dbbb1b0fec2758b33838
moshuss <113644739+moshuss@users.noreply.github.com>
2024-03-12 13:00:45 -0700

    Update projectPosting.php

--- projectPosting.php
+++ projectPosting.php
@@ -11,22 +11,26 @@
 		function addFunderField(num){
 			const s = `<br>	<label for="funder${num}">Funder ${num}:</label>
 							<input type="text" id="funder${num}" name="funder${num}" required>
+							&emsp;
+							<label for="fundingTotal${num}">Funding Total: $</label>
+            				<input type="number" id="funding${num}" name="funding${num}" min="1" step="any" pattern="^\d+(\.)\d{2}$" required>
+            				<input type="checkbox" id="yearly${num}" name="yearly${num}" value="yearly${num}">
+            				<label for="yearly${num}">yearly</label>
+							&emsp;
                 			<span id="addFunder${num+1}">
                 				<a onclick="addFunderField(${num+1});return false;" href="#">+add</a>
                 			</span>
 			`;
   			document.getElementById(`addFunder${num}`).innerHTML = s;
 		};
-		function addResearcherField(num){
-			const s = `<br>	<label for="researcher${num}">Researcher ${num}:</label>
-							<input type="text" id="researcher${num}" name="researcher${num}" required>
-							<input type="checkbox" id="lead" name="lead" value="yesLead">
-      						<label for="lead"> Lead</label>
-                			<span id="addResearcher${num+1}">
-                				<a onclick="addResearcherField(${num+1});return false;" href="#">+add</a>
+		function addCoResearcherField(num){
+			const s = `<br>	<label for="coResearcher${num}">Co-Researcher ${num}:</label>
+							<input type="text" id="coResearcher${num}" name="coResearcher${num}" required>
+                			<span id="addCoResearcher${num+1}">
+                				<a onclick="addCoResearcherField(${num+1});return false;" href="#">+add</a>
                 			</span>
 			`;
-  			document.getElementById(`addResearcher${num}`).innerHTML = s;
+  			document.getElementById(`addCoResearcher${num}`).innerHTML = s;
 		};
 		function addStudentField(num){
 			const s = `<br>	<label for="student${num}">Student ${num}:</label>
@@ -37,21 +41,77 @@
 			`;
   			document.getElementById(`addStudent${num}`).innerHTML = s;
 		};
+		function addContractorField(num){
+			const s = `<br>	<label for="contractor${num}">Contractor ${num}:</label>
+							<input type="text" id="contractor${num}" name="contractor${num}" required>
+                			<span id="addContractor${num+1}">
+                				<a onclick="addContractorField(${num+1});return false;" href="#">+add</a>
+                			</span>
+			`;
+  			document.getElementById(`addContractor${num}`).innerHTML = s;
+		};
+		function addClientField(num){
+			const s = `<br>	<label for="client${num}">Client ${num}:</label>
+							<input type="text" id="client${num}" name="client${num}" required>
+                			<span id="addClient${num+1}">
+                				<a onclick="addClientField(${num+1});return false;" href="#">+add</a>
+                			</span>
+			`;
+  			document.getElementById(`addClient${num}`).innerHTML = s;
+		};
 	</script>
 	</head>
 	<body onload='displayHeader()'>
   		<div class='header' id='header'></div>
-		
 		<h1>Create Project</h1>
+		<form method="post" action="#">
+			<fieldset>
+				<strong>Add Person:</strong>
+				&emsp;
+    			<label for="salutation">Salutation:</label>
+                <select name="salutation" id="salutation">
+                    <option value="mr">Mr.</option>
+                    <option value="mrs">Mrs.</option>
+                    <option value="miss">Miss</option>
+                    <option value="ms">Ms.</option>
+                    <option value="dr">Dr.</option>
+                </select>
+				&emsp;
+				<label for="firstName">First Name:</label>
+				<input type="text" id="firstName" name="firstName">
+				&emsp;
+				<label for="lastName">Last Name:</label>
+				<input type="text" id="lastName" name="lastName">
+				&emsp;
+				<label for="contactEmail">Email:</label>
+				<input type="email" id="contactEmail" name="contactEmail" required>
+				&emsp;
+				<input type="submit" value="Add Person">
+			</fieldset>
+    	</form>
+    	<form method="post" action="#">
+			<fieldset>
+				<strong>Add Business:</strong>
+				&emsp;
+				<label for="businessName">Business Name:</label>
+				<input type="text" id="businessName" name="businessName">
+				&emsp;
+				<label for="businessEmail">Email:</label>
+				<input type="email" id="businessEmail" name="businessEmail" required>
+				&emsp;
+				<input type="submit" value="Add Business">
+			</fieldset>
+    	</form>	
 		<form method="post" action="projectList.php">
 			<fieldset>
 				<h3>Project Details</h3>
+				<br>
 				<label for="projectCode">Project Code:</label>
 				<input type="text" id="projectCode" name="projectCode" required>
-				<br><br>
+				&emsp;
 				<label for="title">Title:</label>
 				<input type="text" id="title" name="title" required>
-				<br><br>
+				&emsp;
 				<label for="stage">Project Stage:</label>
                 <select name="stage" id="stage">
                     <option value="ideation">Ideation</option>
@@ -63,65 +123,92 @@
                 </select>
 				<br><br>
 				<label for="description">Description:</label><br>
-				<textarea id="description" name="description" rows="4" cols="55" required></textarea>
+				<textarea id="description" name="description" rows="4" cols="100" required></textarea>
 				<br><br>
 				<label for="projectType">Project Type:</label>
-                <select name="projectType" id="projectType">
-                    <option value="community">Community</option>
-                    <option value="technical">Technical</option>
-                    <option value="business">Business</option>
-                    <option value="other">Other...</option>
-                </select>
+				<input list="projectTypes" name="projectType" id="projectType">
+                <datalist id="projectTypes">
+                    <option value="Community">
+                    <option value="Technical">
+                    <option value="Business">
+                </datalist>
+                <br><br>
+				<label for="projectManager">Project Manager:</label>
+				<input type="text" id="projectManager" name="projectManager" required>
 				<br><br>
 				<div id="funders">
-    				<label for="funder1">Funder 1:</label>
+    				<label for="funder1">Funder:</label>
     				<input type="text" id="funder1" name="funder1" required>
+    				&emsp;
+    				<label for="fundingTotal">Funding Total: $</label>
+    				<input type="number" id="funding" name="funding" min="1" step="any" pattern="^\d+(\.)\d{2}$" required>
+    				<input type="checkbox" id="yearly1" name="yearly1" value="yearly1">
+    				<label for="yearly1">yearly</label>
+    				&emsp;
     				<span id='addFunder2'>
     					<a onclick="addFunderField(2);return false;" href="#">+add</a>
     				</span>
     			</div>
 				<br>
-				<label for="fundingTotal">Funding Total: $</label>
-				<input type="number" id="funder" name="funder" min="1" step="any" pattern="^\d+(\.)\d{2}$" required>
-				<input type="radio" id="yearly" name="frequency" value="yearly">
-				<label for="yearly">yearly</label>
-				<input type="radio" id="monthly" name="frequency" value="monthly">
-				<label for="monthly">monthly</label>
-				<br><br>
 				<label for="startDate">Start Date:</label>
 				<input type="date" id="startDate" name="startDate" required>&emsp;
 				<label for="endDate">End Date:</label>
 				<input type="date" id="endDate" name="endDate" required>
 				<br><br>
-				<label for="client">Client:</label>
-				<input type="text" id="client" name="client" required>
+				<input type="submit" value="Create Project" style="width:100px">		
+				<br><br>
+			</fieldset>
+		</form>
+		<form  method="post" action="#">
+			<fieldset>
+				<h3>Activity Details</h3>
+				<br>
+				<label for="forProjectCode">Project Code:</label>
+				<input type="text" id="forProjectCode" name="forProjectCode" required>
+				&emsp;
+				<label for="activityCode">Activity Code:</label>
+				<input type="text" id="activityCode" name="activityCode" required>
+				<br><br>
+				<div id="clients">
+    				<label for="client1">Client:</label>
+    				<input type="text" id="client" name="client1" required>
+    				<span id='addClient2'>
+        				<a onclick="addClientField(2);return false;" href="#">+add</a>
+        			</span>
+    			</div>
+				<br>
+				<label for="principleResearcher">Principle Researcher:</label>
+				<input type="text" id="principleResearcher" name="principleResearcher" required>
 				<br><br>
 				<div id="researchers">
-    				<label for="reasercher1">Researcher 1:</label>
-    				<input type="text" id="reasercher1" name="researcher1" required>
-    				<input type="checkbox" id="lead" name="lead" value="yesLead">
-      				<label for="lead"> Lead</label>
-  					<span id='addResearcher2'>
-    					<a onclick="addResearcherField(2);return false;" href="#">+add</a>
+    				<label for="coResearcher1">Co-Researcher:</label>
+    				<input type="text" id="coResearcher1" name="coResearcher1" required>
+  					<span id='addCoResearcher2'>
+    					<a onclick="addCoResearcherField(2);return false;" href="#">+add</a>
     				</span>
     			</div>
-				<br><br>
+				<br>
 				<div id="students">
-    				<label for="student1">Student 1:</label>
+    				<label for="student1">Student:</label>
     				<input type="text" id="student1" name="student1" required>
     				<span id='addStudent2'>
     					<a onclick="addStudentField(2);return false;" href="#">+add</a>
     				</span>
 				</div>
-				<br><br>
-				<label for="contractor">Contractor:</label>
-				<input type="text" id="contractor" name="contractor" required>
-				<br><br>
+				<br>
+				<div id="contractors">
+    				<label for="contractor1">Contractor:</label>
+    				<input type="text" id="contractor1" name="contractor1" required>
+    				<span id='addContractor2'>
+        				<a onclick="addContractorField(2);return false;" href="#">+add</a>
+        			</span>
+    			</div>
+				<br>
 				<label for="notes">Notes:</label><br>
-				<textarea id="notes" name="notes" rows="8" cols="55"></textarea>
+				<textarea id="notes" name="notes" rows="8" cols="110"></textarea>
 				<br><br>
-				<input type="submit" value="Create Project">
-					
+				<input type="submit" value="Add Activity" style="width:100px">	
+				<br><br>	
 			</fieldset>
 		</form>
 	</body>
