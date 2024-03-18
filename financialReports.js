1cd1aea145e04ce16c5e0e0aff1e7104b3e23243
Cody <cjorgenson39@gmail.com>
2024-03-18 12:18:57 -0700

    work in progress but it outputs to a file

--- financialReports.js
+++ financialReports.js
@@ -1,6 +1,7 @@
-function displayByYear(div, year, query){
+function displayByYear(div, start, end, query){
 	var xmlhttp = new XMLHttpRequest();
-	var yearNum = document.getElementById(year).value;
+	 start = document.getElementById(start).value;
+	 end = document.getElementById(end).value;
 	
 	xmlhttp.onreadystatechange = function() {
 		console.log("Ready state: " + xmlhttp.readyState + ", status: " + xmlhttp.status);
@@ -12,6 +13,27 @@ function displayByYear(div, year, query){
 			document.getElementById(div).innerHTML = "Error ocurred.";
 		}
 	}
-	xmlhttp.open("GET", "financialQueries.php?year="+yearNum+"&query="+query,true);
+	//console.log()
+	xmlhttp.open("GET", "financialQueries.php?start="+start+"&end="+end+"&query="+query,true);
+	xmlhttp.send();
+}
+
+function displayCSV (){
+	var xmlhttp = new XMLHttpRequest();
+	window.open(href, "csvOutput.html");
+	var div = "csvOutput";
+	
+	xmlhttp.onreadystatechange = function() {
+		console.log("Ready state: " + xmlhttp.readyState + ", status: " + xmlhttp.status);
+		if(xmlhttp.readyState != 4 && xmlhttp.status == 200){
+			document.getElementById(div).innerHTML = "Checking..";
+		}else if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+			document.getElementById(div).innerHTML = xmlhttp.responseText;
+		}else {
+			document.getElementById(div).innerHTML = "Error ocurred.";
+		}
+	}	
+	
+	xmlhttp.open("GET", "readCSV.php",true);
 	xmlhttp.send();
 }
