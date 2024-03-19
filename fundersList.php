49092410f94f2e5593a0024d01a6f7cf57f6ea0e
Hayden Nikkel <hayden.nikkel@myokanagan.bc.ca>
2024-03-19 13:10:35 -0700

    fixed and changed fundersList.php

--- fundersList.php
+++ fundersList.php
@@ -30,58 +30,59 @@
 <h1>Funders</h1>
 <table>
   <tr>
-  	<th>Funder Salutation</th>
+  	<!--<th>Funder Salutation</th>
     <th>Funder First Name</th>
-    <th>Funder Last Name</th>
+    <th>Funder Last Name</th>-->
     <th>Funder Company</th>
     <th>Funder Email</th> <!-- entity table email -->
     <th>Project Code</th> <!-- projectcode -->
+    <th>Project Name</th> <!-- title -->
     <th>Date Received</th> <!-- dateReceived -->
-    <th>Frequency</th> <!-- depending on int value, Bi-Yearly, Yearly or One-Time  -->
+    <th>Frequency</th> <!-- depending on int value, Yearly or One-Time  -->
     <th>Funding Amount</th> <!-- totalFunding -->
-    <!-- <th>Total Funding Amount</th> totalfunding across every funder row with the same projectcode and entityid -->
   </tr>
 <?php
-//$type = ["Community", "Technical", "Business", "Other"]; might need this...
 
 $sqli = new mysqli('localhost:3306','root','','Research');
-$fullquery = "SELECT NVL(e.salutation, 'N/A') salutation, 
-              NVL(e.first_name, 'N/A') firstname,
-              NVL(e.last_name, 'N/A') lastname,
-              NVL(e.company, 'N/A') company,
+$fullquery = "SELECT e.company,
               e.email,
               f.project_code,
+              p.title,
               f.date_given,
               f.frequency,
               f.funding_amt
-              FROM Funders f, Entities e
-              WHERE e.id = f.entity_id
+              FROM Funders f, Entities e, Projects p
+              WHERE e.id = f.entity_id AND f.project_code = p.project_code
               ORDER BY f.date_given DESC";
-              //need to do total funding amount...
 
 $result = mysqli_query($sqli, $fullquery);
 if(mysqli_num_rows($result) !== 0){
   while ($row = mysqli_fetch_array($result)) {
-    $salutation = stripslashes($row['salutation']);
-    $firstname = stripslashes($row['firstname']);
-    $lastname = stripslashes($row['lastname']);
+    //$salutation = stripslashes($row['salutation']);
+    //$firstname = stripslashes($row['first_name']);
+    //$lastname = stripslashes($row['last_name']);
     $company = stripslashes($row['company']);
     $email = stripslashes($row['email']);
     $projectCode = stripslashes($row['project_code']);
+    $projectTitle = stripslashes($row['title']);
     $dateGiven = stripslashes($row['date_given']);
-    $frequency = stripslashes($row['frequency']);
+    //$frequency = stripslashes($row['frequency']);
     $fundingAmt = stripslashes($row['funding_amt']);
 
+    if(stripslashes($row['frequency'] == 1)){
+      $frequency = 'One-Time';
+    }else if(stripslashes($row['frequency'] == 2)){
+      $frequency = 'Yearly';
+    }
+    //project page link is WIP... send project_code via GET...
     $entry = "<tr>
-                <td>".$salutation."</td>
-                <td>".$firstname."</td>
-                <td>".$lastname."</td>
                 <td>".$company."</td>
                 <td>".$email."</td>
                 <td>".$projectCode."</td>
+                <td><a href='projectPage.php'>".$projectTitle."</a></td>
                 <td>".$dateGiven."</td>
                 <td>".$frequency."</td>
-                <td>".$fundingAmt."</td>
+                <td>$".$fundingAmt."</td>
               </tr>";
     echo $entry;
   }
