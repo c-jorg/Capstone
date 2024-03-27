ef8c8f66fd2542d50019c12fb6d8eadb03331402
moshuss <113644739+moshuss@users.noreply.github.com>
2024-03-27 14:12:38 -0700

    Working Project section with Edit and save
    Please do not "remove"  from the middle

--- Classes/Client.php
+++ Classes/Client.php
@@ -45,10 +45,10 @@ class Client {
         $this->closeConnection();
     }
 
-    public function updateClient($entity_id, $project_code) {
+    public function updateClient($entity, $project) {
         $this->deleteClient();
-        $this->entity = new Entity($entity_id);
-        $this->project = new Project($project_code);
+        $this->entity = $entity;
+        $this->project = $project;
         $this->insertClient();
         
     }
